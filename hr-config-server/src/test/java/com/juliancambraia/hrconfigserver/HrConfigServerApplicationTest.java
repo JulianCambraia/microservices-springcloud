f2de712f6d91fce4817a51fe54073790ccf05d3f
JulianCambraia <juliancambraia@gmail.com>
2021-01-31 12:13:39 -0300

    Containers Dockerfile hr-config-server, hr-eureka-server.

---
+++ hr-config-server/src/test/java/com/juliancambraia/hrconfigserver/HrConfigServerApplicationTest.java
@@ -0,0 +1,19 @@
+package com.juliancambraia.hrconfigserver;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HrConfigServerApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void run() {
+    }
+}
