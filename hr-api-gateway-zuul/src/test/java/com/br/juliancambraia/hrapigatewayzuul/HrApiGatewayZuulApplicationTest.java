88006213ec8bed52ab1b6d555967b1962f3eac57
JulianCambraia <juliancambraia@gmail.com>
2021-01-31 23:23:20 -0300

    Containers Dockerfile hr-api-gateway-zuul.

---
+++ hr-api-gateway-zuul/src/test/java/com/br/juliancambraia/hrapigatewayzuul/HrApiGatewayZuulApplicationTest.java
@@ -0,0 +1,15 @@
+package com.br.juliancambraia.hrapigatewayzuul;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HrApiGatewayZuulApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+    }
+}
