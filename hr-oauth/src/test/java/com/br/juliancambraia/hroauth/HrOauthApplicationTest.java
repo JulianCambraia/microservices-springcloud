5a8dd5206c408d6788f3260e9a92640daa36abbb
JulianCambraia <juliancambraia@gmail.com>
2021-01-31 23:08:04 -0300

    Containers Dockerfile hr-oauth.

---
+++ hr-oauth/src/test/java/com/br/juliancambraia/hroauth/HrOauthApplicationTest.java
@@ -0,0 +1,15 @@
+package com.br.juliancambraia.hroauth;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HrOauthApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+    }
+}
